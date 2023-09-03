1a24c9d097b85125365b5a43f0b52aa11fb80d60
kineticKshitij <143507517+kineticKshitij@users.noreply.github.com>
2023-09-03 21:13:13 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,13 @@
+public class Primitive{
+    public static void main(String[] args){
+      boolean b=true;
+      char c='K';
+      byte a=9;
+      short d=98;
+      int e=-65;
+      long f=456585;
+      float g=456;
+      double h=-4221.2365;
+      System.out.println("boolean="+b+"\nbyte="+a+"\nchar="+c+"\nshort="+d+"\nint="+e+"\nlong="+f+"\nfloat="+g+"\ndouble="+h);
+    }
+}
